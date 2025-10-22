df2dde15a761152edd744f4b8030295ce80e7c1c
Amir Yeganemehr <ygnmehr@gmail.com>
2025-10-22 14:30:44 +0330

    refactor: replace grammar name check with grammar type check

--- src/LaravelModelUuidServiceProvider.php
+++ src/LaravelModelUuidServiceProvider.php
@@ -6,6 +6,9 @@ use Dyrynda\Database\Support\Exceptions\UnknownGrammarClass;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Schema\ColumnDefinition;
 use Illuminate\Database\Schema\Grammars\Grammar;
+use Illuminate\Database\Schema\Grammars\MySqlGrammar;
+use Illuminate\Database\Schema\Grammars\PostgresGrammar;
+use Illuminate\Database\Schema\Grammars\SQLiteGrammar;
 use Illuminate\Support\Fluent;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
@@ -26,12 +29,17 @@ class LaravelModelUuidServiceProvider extends PackageServiceProvider
     public function packageRegistered()
     {
         Grammar::macro('typeEfficientUuid', function (Fluent $column) {
-            return match (class_basename(static::class)) {
-                'MySqlGrammar' => sprintf('binary(%d)', $column->length ?? 16),
-                'PostgresGrammar' => 'bytea',
-                'SQLiteGrammar' => 'blob(256)',
-                default => throw new UnknownGrammarClass
-            };
+            if ($this instanceof MySqlGrammar) {
+                return sprintf('binary(%d)', $column->length ?? 16);
+            }
+            if ($this instanceof PostgresGrammar) {
+                return 'bytea';
+            }
+            if ($this instanceof SQLiteGrammar) {
+                return 'blob(256)';
+            }
+
+            throw new UnknownGrammarClass;
         });
 
         Blueprint::macro('efficientUuid', function ($column): ColumnDefinition {
